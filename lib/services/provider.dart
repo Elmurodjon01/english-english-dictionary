1647d2f910a4b6aad40e95da27bcd8b80c9b7870
abdullajonovelmurodjon <elmurodjon@inha.edu>
2022-10-23 23:52:24 +0900

    night mode on process

--- lib/services/provider.dart
+++ lib/services/provider.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 
 class DictProvider extends ChangeNotifier {
   int _i = 0;
+  late bool _isNight = false;
+  bool get isNight => _isNight;
   int get initialIndex => _i;
   late  bool _searchActive = false;
   bool get isSearchActive => _searchActive;
@@ -13,6 +15,10 @@ class DictProvider extends ChangeNotifier {
   }
   void searchBrain (bool _new){
     _searchActive = _new;
+    notifyListeners();
+  }
+  void nightMode (bool switchOnOFF){
+    _isNight = switchOnOFF;
     notifyListeners();
   }
 
