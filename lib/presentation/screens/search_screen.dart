acd6890bb3963d73d58f38ab13f20641e12af81f
abdullajonovelmurodjon <elmurodjon@inha.edu>
2022-10-27 22:29:09 +0900

    icon has been added for both ios and android

--- lib/presentation/screens/search_screen.dart
+++ lib/presentation/screens/search_screen.dart
@@ -23,9 +23,9 @@ class _SearchScreenState extends State<SearchScreen> {
   }
 
   void playAudio(var music) {
-    audioPlayer!.stop();
+    audioPlayer?.stop();
 
-    audioPlayer!.play(music);
+    audioPlayer?.play(music);
   }
 
   @override
@@ -42,172 +42,172 @@ class _SearchScreenState extends State<SearchScreen> {
       backgroundColor: Colors.blue,
       body: SafeArea(
         child: CustomScrollView(
-            slivers: [
-              SliverToBoxAdapter(
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  height: MediaQuery.of(context).size.height,
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.only(left: 40),
-                            width: _width * 0.6,
-                            height: _height * 0.05,
-                            decoration: const BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(15),
-                                bottomLeft: Radius.circular(15),
-                              ),
+          slivers: [
+            SliverToBoxAdapter(
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                height: MediaQuery.of(context).size.height,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(left: 40),
+                          width: _width * 0.6,
+                          height: _height * 0.05,
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(15),
+                              bottomLeft: Radius.circular(15),
                             ),
-                            child: TextFormField(
-                              // onTap: searchPop,
-                              controller: controller,
-                              decoration: const InputDecoration(
-                                hintText: 'Type a word to search',
-                                border: InputBorder.none,
-                                contentPadding: EdgeInsets.only(
-                                  left: 24.0,
-                                ),
+                          ),
+                          child: TextFormField(
+                            // onTap: searchPop,
+                            controller: controller,
+                            decoration: const InputDecoration(
+                              hintText: 'Type a word to search',
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.only(
+                                left: 24.0,
                               ),
                             ),
                           ),
-                          const SizedBox(
-                            width: 13,
-                          ),
-                          Container(
-                            padding: const EdgeInsets.only(left: 5),
-                            height: _height * 0.05,
-                            width: _width * 0.1,
-                            decoration: const BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.only(
-                                topRight: Radius.circular(15),
-                                bottomRight: Radius.circular(15),
-                              ),
-                            ),
-                            child: ColorSonar(
-                              contentAreaRadius: 1,
-                              waveFall: 6.0,
-                              waveMotion: WaveMotion.synced,
-                              child: IconButton(
-                                icon: const Icon(
-                                  Icons.search_sharp,
-                                  color: Color.fromARGB(255, 9, 1, 41),
-                                ),
-                                onPressed: () {
-                                  if (controller.text.isNotEmpty) {
-                                    setState(() {});
-                                  }
-                                },
-                              ),
+                        ),
+                        const SizedBox(
+                          width: 13,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(left: 5),
+                          height: _height * 0.05,
+                          width: _width * 0.1,
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(15),
+                              bottomRight: Radius.circular(15),
                             ),
                           ),
-                        ],
-                      ),
-
-                      // Container(
-                      //   padding: const EdgeInsets.symmetric(horizontal: 10),
-                      //   decoration: BoxDecoration(
-                      //     borderRadius: BorderRadius.circular(10),
-                      //     border: Border.all(),
-                      //   ),
-                      //   child: Row(
-                      //     children: [
-                      //       Expanded(
-                      //         child: TextFormField(
-                      //           controller: controller,
-                      //           decoration: const InputDecoration(
-                      //             label: Text('Search Query'),
-                      //             border: InputBorder.none,
-                      //           ),
-                      //         ),
-                      //       ),
-                      //       IconButton(
-                      //         onPressed: () {
-                      //           if (controller.text.isNotEmpty) {
-                      //             setState(() {});
-                      //           }
-                      //         },
-                      //         icon: const Icon(Icons.search),
-                      //       )
-                      //     ],
-                      //   ),
-                      // ),
-
-                      const SizedBox(
-                        height: 20,
-                      ),
-
-                      ///FutureBuilder
-                      controller.text.isEmpty
-                          ? const SizedBox(child: Text('Search for something'))
-                          : FutureBuilder(
-                              future:
-                                  DictService().searchWord(word: controller.text),
-                              builder: (context,
-                                  AsyncSnapshot<List<DictModel>> snapshot) {
-                                print("Data $snapshot");
-                                if (snapshot.hasData) {
-                                  return Expanded(
-                                    child: ListView(
-                                      children: List.generate(
-                                          snapshot.data!.length, (index) {
-                                        final data = snapshot.data![index];
-                                        return Container(
-                                          child: Column(
-                                            children: [
-                                              Container(
-                                                child: ListTile(
-                                                  title: Text(data.word!),
-                                                  subtitle: Text(data
-                                                      .phonetics![index].text!),
-                                                  trailing: IconButton(
-                                                      onPressed: () {
-                                                        final path = data
-                                                            .phonetics![index]
-                                                            .audio;
-                                                        playAudio(UrlSource(
-                                                            'https:$path'));
-                                                        // playAudio("");
-                                                      },
-                                                      icon: const Icon(
-                                                          Icons.audiotrack)),
-                                                ),
-                                              ),
-                                              Container(
-                                                child: ListTile(
-                                                  title: Text(data
-                                                      .meanings![index]
-                                                      .definitions![index]
-                                                      .definition!),
-                                                  subtitle: Text(data
-                                                      .meanings![index]
-                                                      .partOfSpeech!),
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        );
-                                      }),
-                                    ),
-                                  );
-                                } else if (snapshot.hasError) {
-                                  return Text(snapshot.error.toString());
-                                } else {
-                                  return const CircularProgressIndicator();
+                          child: ColorSonar(
+                            contentAreaRadius: 1,
+                            waveFall: 6.0,
+                            waveMotion: WaveMotion.synced,
+                            child: IconButton(
+                              icon: const Icon(
+                                Icons.search_sharp,
+                                color: Color.fromARGB(255, 9, 1, 41),
+                              ),
+                              onPressed: () {
+                                if (controller.text.isNotEmpty) {
+                                  setState(() {});
                                 }
                               },
-                            )
-                    ],
-                  ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    // Container(
+                    //   padding: const EdgeInsets.symmetric(horizontal: 10),
+                    //   decoration: BoxDecoration(
+                    //     borderRadius: BorderRadius.circular(10),
+                    //     border: Border.all(),
+                    //   ),
+                    //   child: Row(
+                    //     children: [
+                    //       Expanded(
+                    //         child: TextFormField(
+                    //           controller: controller,
+                    //           decoration: const InputDecoration(
+                    //             label: Text('Search Query'),
+                    //             border: InputBorder.none,
+                    //           ),
+                    //         ),
+                    //       ),
+                    //       IconButton(
+                    //         onPressed: () {
+                    //           if (controller.text.isNotEmpty) {
+                    //             setState(() {});
+                    //           }
+                    //         },
+                    //         icon: const Icon(Icons.search),
+                    //       )
+                    //     ],
+                    //   ),
+                    // ),
+
+                    const SizedBox(
+                      height: 20,
+                    ),
+
+                    ///FutureBuilder
+                    controller.text.isEmpty
+                        ? const SizedBox(child: Text('Search for something'))
+                        : FutureBuilder(
+                            future:
+                                DictService().searchWord(word: controller.text),
+                            builder: (context,
+                                AsyncSnapshot<List<DictModel>> snapshot) {
+                              print("Data $snapshot");
+                              if (snapshot.hasData) {
+                                return Expanded(
+                                  child: ListView(
+                                    children: List.generate(
+                                        snapshot.data!.length, (index) {
+                                      final data = snapshot.data![index];
+                                      return Container(
+                                        child: Column(
+                                          children: [
+                                            Container(
+                                              child: ListTile(
+                                                title: Text(data.word!),
+                                                subtitle: Text(data
+                                                    .phonetics![index].text!),
+                                                trailing: IconButton(
+                                                    onPressed: () {
+                                                      final path = data
+                                                          .phonetics![index]
+                                                          .audio;
+                                                      playAudio(UrlSource(
+                                                          'https:$path'));
+                                                      // playAudio("");
+                                                    },
+                                                    icon: const Icon(
+                                                        Icons.audiotrack)),
+                                              ),
+                                            ),
+                                            Container(
+                                              child: ListTile(
+                                                title: Text(data
+                                                    .meanings![index]
+                                                    .definitions![index]
+                                                    .definition!),
+                                                subtitle: Text(data
+                                                    .meanings![index]
+                                                    .partOfSpeech!),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      );
+                                    }),
+                                  ),
+                                );
+                              } else if (snapshot.hasError) {
+                                return Text(snapshot.error.toString());
+                              } else {
+                                return const CircularProgressIndicator();
+                              }
+                            },
+                          )
+                  ],
                 ),
-              )
-            ],
-          ),
+              ),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
